2e6aead106ef8bcedc8576bd7a4e9d4ca4a4ad89
Iago Brayham <iagobrayhamcunha@hotmail.com>
2022-01-17 20:44:23 -0300

    get all todos from collection

--- controllers/todo.go
+++ controllers/todo.go
@@ -2,86 +2,122 @@ package controllers
 
 import (
 	"backend/my-little-kanvas/models"
+	"context"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"gopkg.in/mgo.v2"
+	"go.mongodb.org/mongo-driver/mongo"
 	"gopkg.in/mgo.v2/bson"
 )
 
 type TodoController struct {
-	session *mgo.Session
+	collection *mongo.Collection
+	ctx context.Context
 }
 
-func GetController(session *mgo.Session) *TodoController{
-	return &TodoController{session}
+func GetController(todoCollection *mongo.Collection, ctx context.Context) *TodoController{
+	return &TodoController{collection: todoCollection, ctx: ctx}
 }
 
 // structured functions
-func (todoController TodoController) GetTodos (c *gin.Context) {
+func (tc TodoController) GetTodos (c *gin.Context) {
+	// find all todos
+	var todos []models.Todo
 
-}
-
-func (todoController TodoController) CreateTodo (c *gin.Context) {
-
-}
-
-func (todoController TodoController) GetTodo (c *gin.Context) {
-	// assign the id param of the request to a variable
-	id := c.Param("id")
-
-	// loops through todos array and check for id
-	if !bson.IsObjectIdHex(id) {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
-		return
-	}
-
-	oid := bson.ObjectIdHex(id)
-
-	// create empty object of type Todo
-	todo := models.Todo{}
-
-	// find todo by id and bind it to todo model variable
 	// handle error if given
-	if err := todoController.session.DB("my-little-kanban").C("todos").FindId(oid).One(&todo); err != nil {
+	cursor, err := tc.collection.Find(tc.ctx, bson.M{})
+	
+	if err != nil {
+		log.Fatal(err)
 		// if nothing found, returns JSON object with message
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message":"todo not found"})
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message":"InternalError"})
 		return
 	}
 
+	for cursor.Next(tc.ctx) {
+		var todo models.Todo
+		if err = cursor.Decode(&todo); err != nil {
+			log.Fatal(err)
+		}
+		todos = append(todos, todo)
+	}
 	// if found, returns JSON object of todo
-	c.IndentedJSON(http.StatusOK, todo)
+	c.IndentedJSON(http.StatusOK, todos)
+	defer cursor.Close(tc.ctx)
 }
 
-func (todoController TodoController) DeleteTodo (c *gin.Context) {
-
-}
-
-
-
-
-// func getTodos(c *gin.Context) {
-// 	// gets context from gin, like Req/Res from express
-// 	// returns JSON response with status and object
-// 	c.IndentedJSON(http.StatusOK, todos)
-// }
-
-// func postTodo(c *gin.Context) {
+// func (tc TodoController) CreateTodo (c *gin.Context) {
 // 	// defines new variable of type todo
-// 	var newTodo Todo
+// 	var newTodo models.Todo
 
 // 	// binds the body of the request to newTodo variable
 // 	// as it might throw error, check if binding returns error before moving on
 // 	if err := c.BindJSON((&newTodo)); err != nil {
+// 		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message":"Internal error"})
 // 		return
 // 	}
 
-// 	// reassign variable with new object added to it
-// 	todos = append(todos, newTodo)
+// 	// get new _id from mongo
+// 	newTodo.Id = bson.NewObjectId()
+
+// 	// try to insert it to db
+// 	if err := tc.session.DB("my-little-kanban").C("todos").Insert(newTodo); err != nil {
+// 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not found"})
+// 		return
+// 	}
+	
 // 	// returns JSON response
 // 	c.IndentedJSON(http.StatusCreated, newTodo)
 // }
 
-// func getTodoByID(c *gin.Context) {
-	
+// func (tc TodoController) GetTodo (c *gin.Context) {
+// 	// assign the id param of the request to a variable
+// 	id := c.Param("id")
+
+// 	// loops through todos array and check for id
+// 	if !bson.IsObjectIdHex(id) {
+// 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+// 		return
+// 	}
+
+// 	// convert string hex to object id type
+// 	oid := bson.ObjectIdHex(id)
+
+// 	// create empty object of type Todo
+// 	todo := models.Todo{}
+
+// 	// find todo by id and bind it to todo model variable
+// 	// handle error if given
+// 	if err := tc.session.DB("my-little-kanban").C("todos").FindId(oid).One(&todo); err != nil {
+// 		// if nothing found, returns JSON object with message
+// 		c.IndentedJSON(http.StatusNotFound, gin.H{"message":"todo not found"})
+// 		return
+// 	}
+
+// 	// if found, returns JSON object of todo
+// 	c.IndentedJSON(http.StatusOK, todo)
 // }
+
+// func (tc TodoController) DeleteTodo (c *gin.Context) {
+// 	// assign the id param of the request to a variable
+// 	id := c.Param("id")
+
+// 	// loops through todos array and check for id
+// 	if !bson.IsObjectIdHex(id) {
+// 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not found"})
+// 		return
+// 	}
+
+// 	// convert string hex to object id type
+// 	oid := bson.ObjectIdHex(id)
+
+// 	if err := tc.session.DB("my-little-kanban").C("todos").RemoveId(oid); err != nil {
+// 		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Todo not found"})
+// 		return
+// 	}
+
+// 	c.IndentedJSON(http.StatusOK, gin.H{"message": "Todo deleted", "_id": id})
+// }
+
+
--- main.go
+++ main.go
@@ -4,39 +4,63 @@ package main
 // Import needed libraries
 import (
 	"backend/my-little-kanvas/controllers"
+	"context"
+	"log"
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"gopkg.in/mgo.v2"
-	// "github.com/gin-gonic/gin"
-	// "go.mongodb.org/mongo-driver/mongo"
-	// "go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 // The start function of the module
 func main() {
+	// setup mongo connection
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://iagobrayham:iagobrayham@cluster0.4deg4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10 * time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	err  = client.Ping(ctx, readpref.Primary())
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	database := client.Database("my-little-kanban")
+	todosCollection := database.Collection("todos")
+
 	// I guess this extracts the router object from gin.Default()
 	router := gin.Default()
-	todoController := controllers.GetController(getSession())
+	todoController := controllers.GetController(todosCollection, ctx)
 
 	// Define endpoints and handlers/callbacks
 	router.GET("/todos", todoController.GetTodos)
-	router.POST("/todos", todoController.CreateTodo)
-	router.GET("/todos/:id", todoController.GetTodo)
-	router.DELETE("/todos/:id", todoController.DeleteTodo)
+	// router.POST("/todos", todoController.CreateTodo)
+	// router.GET("/todos/:id", todoController.GetTodo)
+	// router.DELETE("/todos/:id", todoController.DeleteTodo)
 
 	// Tell the server to run on port 8080
 	router.Run("localhost:8080")
+	defer client.Disconnect(ctx)
+	
 }
 
-//  function to return mongo session
-// * astherisks tels the return type
-func getSession() *mgo.Session {
-	// stablish connection to cursor
-	session, err := mgo.DialWithTimeout("mongodb+srv://iagobrayham:iagobrayham@cluster0.h2jsn.mongodb.net", time.Duration(5 * time.Second))
-	// check for error
-	if err != nil {
-		panic(err)
-	}
-	return session
-}
+// function to return mongo session
+// // astherisks tels the return type
+// func getSession() *mgo.Session {
+// 	// stablish connection to cursor
+// 	// session, err := mgo.Dial("mongodb://iagobrayham:iagobrayham@cluster0.4deg4.mongodb.net/my-little-kanban")
+// 	// // // session, err := mgo.Dial("mongodb+srv://iagobrayham:iagobrayham@cluster0.4deg4.mongodb.net/my-little-kanban")
+// 	// // // check for error
+// 	// // if err != nil {
+// 	// // 	panic(err)
+// 	// // }
+// 	// return mgo.Session{}
+// }
